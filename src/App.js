3213fa480e6539249974273ecd33e6f2b603d71d
杉本　諒 <wasp2123321@gmail.com>
2021-10-24 18:58:34 +0900

    Class component to Function component convert

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import {  SEARCH_REPOSITORIES } from './graphql'
 import {ApolloProvider} from 'react-apollo'
 import {Query} from 'react-apollo'
 import { useState } from 'react'
-import { Component } from 'react/cjs/react.production.min'
-import { render } from '@testing-library/react'
 
-const DEFAULT_STATE = {
+const VARIABLES = {
   first: 5,
   after: null,
   last: null,
@@ -14,45 +12,36 @@ const DEFAULT_STATE = {
   query: "フロントエンドエンジニア"
 }
 
-class App extends Component {
+function App() {
+  const [searchResult, setSearchResult] = useState(VARIABLES)
 
-  constructor(props) {
-    super(props)
-    this.state = DEFAULT_STATE
-
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange(e) {
-    this.setState({
-      ...DEFAULT_STATE,
+  const handleChange = (e) => {
+    setSearchResult({
+      ...searchResult,
       query: e.target.value
     })
   }
 
-  render() {
-    const {query, first, last, before, after} = this.state
-    console.log({query})
-    return (
-      <ApolloProvider client={client}>
-        <form>
-          <input value={query} onChange={this.handleChange}/>
-        </form>
-        <Query query={SEARCH_REPOSITORIES}
-        variables={{query, first, last, before, after}}
-        >
-        {
-          ({loading, error, data}) => {
-            if (loading) return "Loading..."
-            if (error) return `Error! ${error.message}`
-            console.log({data})
-            return <div></div>
-          }
+  const {query, first, last, before, after} = searchResult
+  return (
+    <ApolloProvider client={client}>
+      <form>
+        <input value={query} onChange={handleChange}/>
+      </form>
+      <Query query={SEARCH_REPOSITORIES}
+      variables={{query, first, last, before, after}}
+      >
+      {
+        ({loading, error, data}) => {
+          if (loading) return "Loading..."
+          if (error) return `Error! ${error.message}`
+          console.log({data})
+          return <div></div>
         }
-        </Query>
-      </ApolloProvider>
-    );
-  }
+      }
+      </Query>
+    </ApolloProvider>
+  );
 }
 
 export default App;
