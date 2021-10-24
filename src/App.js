f231882a98c5fda4e08b936587405a87fca745aa
杉本　諒 <wasp2123321@gmail.com>
2021-10-24 18:04:28 +0900

    GraphQLクライアントをセットアップする

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+import client from './client'
+import {ApolloProvider} from 'react-apollo'
+import gql from 'graphql-tag'
+import {Query} from 'react-apollo'
+
+const ME = gql`
+  query me {
+    user(login: "waspryo") {
+      name,
+      avatarUrl
+    }
+  }
+`
+
 function App() {
   return (
-    <div>
-    </div>
+    <ApolloProvider client={client}>
+      <div>Graphql</div>
+      <Query query={ME}>
+      {
+        ({loading, error, data}) => {
+          if (loading) return "Loading..."
+          if (error) return `Error! ${error.message}`
+
+          return <div>{data.user.name}</div>
+        }
+      }
+      </Query>
+    </ApolloProvider>
   );
 }
 
