449a7eeba862ceaea65c118a95587ec9aa64e43e
杉本　諒 <wasp2123321@gmail.com>
2021-10-24 18:50:32 +0900

    動的に検索結果を変える

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import client from './client'
-import { ME, SEARCH_REPOSITORIES } from './graphql'
+import {  SEARCH_REPOSITORIES } from './graphql'
 import {ApolloProvider} from 'react-apollo'
 import {Query} from 'react-apollo'
+import { useState } from 'react'
+import { Component } from 'react/cjs/react.production.min'
+import { render } from '@testing-library/react'
 
-const VARIABLES = {
+const DEFAULT_STATE = {
   first: 5,
   after: null,
   last: null,
@@ -11,24 +14,45 @@ const VARIABLES = {
   query: "フロントエンドエンジニア"
 }
 
-function App() {
-  const {query, first, last, before, after} = VARIABLES
-  return (
-    <ApolloProvider client={client}>
-      <Query query={SEARCH_REPOSITORIES}
-      variables={{query, first, last, before, after}}
-      >
-      {
-        ({loading, error, data}) => {
-          if (loading) return "Loading..."
-          if (error) return `Error! ${error.message}`
-          console.log({data})
-          return <div></div>
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = DEFAULT_STATE
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({
+      ...DEFAULT_STATE,
+      query: e.target.value
+    })
+  }
+
+  render() {
+    const {query, first, last, before, after} = this.state
+    console.log({query})
+    return (
+      <ApolloProvider client={client}>
+        <form>
+          <input value={query} onChange={this.handleChange}/>
+        </form>
+        <Query query={SEARCH_REPOSITORIES}
+        variables={{query, first, last, before, after}}
+        >
+        {
+          ({loading, error, data}) => {
+            if (loading) return "Loading..."
+            if (error) return `Error! ${error.message}`
+            console.log({data})
+            return <div></div>
+          }
         }
-      }
-      </Query>
-    </ApolloProvider>
-  );
+        </Query>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
