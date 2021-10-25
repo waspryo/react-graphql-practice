659cea9cd739b3d8abffa6eaefcb5723a6f66c2a
杉本　諒 <wasp2123321@gmail.com>
2021-10-25 10:53:12 +0900

    検索結果一覧をリスト表示する

--- src/App.js
+++ src/App.js
@@ -1,47 +1,66 @@
-import client from './client'
-import {  SEARCH_REPOSITORIES } from './graphql'
-import {ApolloProvider} from 'react-apollo'
-import {Query} from 'react-apollo'
-import { useState } from 'react'
+import client from "./client";
+import { SEARCH_REPOSITORIES } from "./graphql";
+import { ApolloProvider } from "react-apollo";
+import { Query } from "react-apollo";
+import { useState } from "react";
 
 const VARIABLES = {
   first: 5,
   after: null,
   last: null,
   before: null,
-  query: "フロントエンドエンジニア"
-}
+  query: "フロントエンドエンジニア",
+};
 
 function App() {
-  const [searchResult, setSearchResult] = useState(VARIABLES)
+  const [searchResult, setSearchResult] = useState(VARIABLES);
 
   const handleChange = (e) => {
     setSearchResult({
       ...searchResult,
-      query: e.target.value
-    })
-  }
+      query: e.target.value,
+    });
+  };
 
-  const {query, first, last, before, after} = searchResult
+  const { query, first, last, before, after } = searchResult;
   return (
     <ApolloProvider client={client}>
       <form>
-        <input value={query} onChange={handleChange}/>
+        <input value={query} onChange={handleChange} />
       </form>
-      <Query query={SEARCH_REPOSITORIES}
-      variables={{query, first, last, before, after}}
+      <Query
+        query={SEARCH_REPOSITORIES}
+        variables={{ query, first, last, before, after }}
       >
-      {
-        ({loading, error, data}) => {
-          if (loading) return "Loading..."
-          if (error) return `Error! ${error.message}`
-          const search = data.search
-          const repositoryCount = search.repositoryCount
-          const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories'
-          const title = `Github Repositories Search Results - ${data.search.repositoryCount} ${repositoryUnit}`
-          return <h2>{title}</h2>
-        }
-      }
+        {({ loading, error, data }) => {
+          if (loading) return "Loading...";
+          if (error) return `Error! ${error.message}`;
+          const search = data.search;
+          const repositoryCount = search.repositoryCount;
+          const repositoryUnit =
+            repositoryCount === 1 ? "Repository" : "Repositories";
+          const title = `Github Repositories Search Results - ${data.search.repositoryCount} ${repositoryUnit}`;
+          return (
+            <>
+              <h2>{title}</h2>
+              <ul>
+                {
+                  // console.log(search.edges)
+                  search.edges.map((edge) => {
+                    const node = edge.node;
+                    return (
+                      <li key={node.id}>
+                        <a href={node.url} target="_blank">
+                          {node.name}
+                        </a>
+                      </li>
+                    );
+                  })
+                }
+              </ul>
+            </>
+          );
+        }}
       </Query>
     </ApolloProvider>
   );
